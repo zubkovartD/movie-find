bc340f3f7ab91111e93d6521da53c921275f2ee5
zubkovartD <36312132+zubkovartD@users.noreply.github.com>
2020-07-11 15:22:38 +0300

    Added login form

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
-import React from 'react';
+import React from "react";
 import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList.jsx";
-import './App.css';
+import Header from "./Header/Header";
+import "./App.css";
 
 export default class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       filters: {
-        sort_by: 'vote_average.desc'
+        sort_by: "vote_average.desc",
       },
-      page: 1
-    }
+      page: 1,
+    };
   }
 
-  onChangeFilters = event => {
+  onChangeFilters = (event) => {
     const newFilters = {
       ...this.state.filters,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     };
-    this.setState({ 
-      filters: newFilters
-    }); 
-  }
-
-  onChangePage = page => {
     this.setState({
-      page
+      filters: newFilters,
     });
-  }
+  };
+
+  onChangePage = (page) => {
+    this.setState({
+      page,
+    });
+  };
 
   render() {
-    const {filters, page} = this.state;
+    const { filters, page } = this.state;
     return (
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-4">
-            <div className="card" style={{ width: "100%" }}>
-              <div className="card-body">
-                <h3>Фильтры:</h3>
-                <Filters 
-                  filters={filters} 
-                  onChangeFilters={this.onChangeFilters} 
-                  page={page}
-                  onChangePage={this.onChangePage}
-                />
+      <>
+        <Header />
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-4">
+              <div className="card" style={{ width: "100%" }}>
+                <div className="card-body">
+                  <h3>Фильтры:</h3>
+                  <Filters
+                    filters={filters}
+                    onChangeFilters={this.onChangeFilters}
+                    page={page}
+                    onChangePage={this.onChangePage}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-8">
-            <MoviesList filters={filters} page={page} onChangePage={this.onChangePage} />
+            <div className="col-8">
+              <MoviesList
+                filters={filters}
+                page={page}
+                onChangePage={this.onChangePage}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
---
+++ src/Header/Login/LoginForm.jsx
@@ -0,0 +1,181 @@
+import React from 'react';
+import {API_KEY_3, API_URL} from './../../Api/api'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class LoginForm extends React.Component {
+    state = {
+        username: "",
+        password: "",
+        errors: {},
+        submitting: false
+    };
+
+    onChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        this.setState(prevState => ({
+            [name]: value,
+            errors: {
+                ...prevState.errors,
+                base: null,
+                [name]: null
+            }
+        }))
+    }
+
+    handleBlur = () => {
+        const errors = this.validateFields() 
+        if (Object.keys(errors).length > 0) {
+            this.setState(prevState => ({
+                errors: {
+                    ...prevState.errors,
+                    ...errors
+                }
+            }))
+        }
+    }
+
+    onSubmit =() => {
+            const fetchAPI = (url, options = {}) => {
+                return new Promise((resolve, reject) => {
+                    fetch(url, options)
+                    .then(response => {
+                        if (response.status < 400){
+                            return response.json()
+                        } else {
+                            throw response
+                        }
+                    })
+                    .then(data => resolve(data))
+                    .catch(response => {
+                        response.json()
+                            .then(error => reject(error))
+                    })
+                })
+            }
+            this.setState({
+                submitting: true
+            })
+            fetchAPI(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
+                .then(data => {
+                    return fetchAPI(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
+                        method: 'POST',
+                        mode: 'cors',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            username: this.state.username, 
+                            password: this.state.password, 
+                            request_token: data.request_token
+                        })
+                    })
+                })
+                .then(data => {
+                    return fetchAPI(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,{
+                        method: 'POST',
+                        mode: 'cors',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            request_token: data.request_token
+                        })
+                    })
+                })
+                .then(data => {
+                    console.log('session', data)
+                    this.setState({
+                        submitting: false
+                    })
+                })
+                .catch(error => {
+                    this.setState({
+                        submitting: false,
+                        errors: {
+                            base: error.status_message
+                        }
+                    })
+                })
+    }
+
+    validateFields = () => {
+        const errors = {}
+
+        if (this.state.username === '') {
+            errors.username = 'Not empty'
+        }
+
+        return errors
+    }
+
+    onLogin = e => {
+        e.preventDefault()
+        const errors = this.validateFields() 
+        if (Object.keys(errors).length > 0) {
+            this.setState(prevState => ({
+                errors: {
+                    ...prevState.errors,
+                    ...errors
+                }
+            }))
+        } else {
+            this.onSubmit()
+        }
+    }
+
+    render() {
+        const { username, password, errors, submitting } = this.state;
+        return (
+            <div className="form-login-container">
+                <form className="form-login">
+                    <h1 className="h3 mb-3 font-weight-normal text-center">
+                        Авторизация
+                    </h1>
+                    <div className="form-group">
+                        <label htmlFor="username">Пользователь</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="username"
+                            placeholder="Пользователь"
+                            name="username"
+                            value={username}
+                            onChange={this.onChange}
+                            onBlur={this.handleBlur}
+                        />
+                        {errors.username && (
+                            <div className="invalid-feedback d-block">{errors.username}</div>
+                        )}      
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Пароль</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            placeholder="Пароль"
+                            name="password"
+                            value={password}
+                            onChange={this.onChange}
+                        />
+                    {errors.password && (
+                            <div className="invalid-feedback d-block">{errors.password}</div>
+                        )}
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-lg btn-primary btn-block"
+                        onClick={this.onLogin}
+                        disabled={submitting}
+                    >
+                        Вход
+                    </button>
+                {errors.base && (
+                        <div className="invalid-feedback d-block text-center">{errors.base}</div>
+                    )} 
+                    </form>
+                </div>
+        );
+    }
+}
